13dc70cf17152af4d644eff00894424d5ea33593
Shaquile Ellard Andhika <100258198+gtvservices5@users.noreply.github.com>
2025-07-09 01:46:18 -0500

    Update add_epg_enrichment_async.py

--- scripts/add_epg_enrichment_async.py
+++ scripts/add_epg_enrichment_async.py
@@ -53,9 +53,9 @@ TMDB_GENRE_MAP = {
 MANUAL_TITLE_OVERRIDES = {
     "Big City Greens": "Big City Greens (TV Series 2018)",
     "Kiff": "Kiff (TV Series 2023)",
-    "Disney's Zombies": "Zombies (2018 film)",
+    "Zombies": "Z-O-M-B-I-E-S",
     "Bluey": "Bluey (TV Series 2018)",
-    "Disney's Jessie": "Jessie (TV Series 2011)"
+    "Jessie": "Jessie (TV Series 2011)"
 }
 
 async def fetch_json(session, url, params):
