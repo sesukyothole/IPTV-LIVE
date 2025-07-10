2d8bed1a5cf65f4063559dc2f5d47808d71a83f5
Shaquile Ellard Andhika <100258198+gtvservices5@users.noreply.github.com>
2025-07-09 23:50:10 -0500

    Update enrich_epg.py

--- scripts/enrich_epg.py
+++ scripts/enrich_epg.py
@@ -37,7 +37,8 @@ MANUAL_ID_OVERRIDES = {
     "Gabby's Dollhouse": {"type": "tv", "id": 111474},
     "black-ish": {"type": "tv", "id": 61381},
     "Phineas and Ferb": {"type": "tv", "id": 1877},
-    "The Amazing World of Gumball": {"type": "tv", "id": 37606}
+    "The Amazing World of Gumball": {"type": "tv", "id": 37606},
+    "Win or Lose": {"type": "tv", "id": 114500}
 }
 
 # Map TMDb genre IDs to names
