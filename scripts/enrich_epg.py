96d3df7ebb563114f9c988201b670f401f11b6d1
Shaquile Ellard Andhika <100258198+gtvservices5@users.noreply.github.com>
2025-07-14 00:14:54 -0500

    Update enrich_epg.py

--- scripts/enrich_epg.py
+++ scripts/enrich_epg.py
@@ -14,7 +14,7 @@ TMDB_IMAGE_BASE = "https://image.tmdb.org/t/p/w500"
 
 TARGET_CHANNELS = {
     "403788", "403674", "403837", "403794", "403620",
-    "403655", "8359", "403847", "403772", "403576"
+    "403655", "8359", "403847", "403461", "403576"
 }
 
 # Manual TMDb ID overrides
@@ -43,10 +43,36 @@ MANUAL_ID_OVERRIDES = {
     "Moana": {"type": "movie", "id": 277834}
 }
 
-# Genre mapping (subset for demo)
 TMDB_GENRES = {
-    16: "Animation", 35: "Comedy", 18: "Drama", 10751: "Family",
-    28: "Action", 12: "Adventure", 10762: "Kids", 10765: "Sci-Fi & Fantasy"
+    28: "Action",
+    12: "Adventure",
+    16: "Animation",
+    35: "Comedy",
+    80: "Crime",
+    99: "Documentary",
+    18: "Drama",
+    10751: "Family",
+    14: "Fantasy",
+    36: "History",
+    27: "Horror",
+    10402: "Music",
+    9648: "Mystery",
+    10749: "Romance",
+    878: "Science Fiction",
+    10770: "TV Movie",
+    53: "Thriller",
+    10752: "War",
+    37: "Western",
+    
+    # TV-specific genres
+    10759: "Action & Adventure",
+    10762: "Kids",
+    10763: "News",
+    10764: "Reality",
+    10765: "Sci-Fi & Fantasy",
+    10766: "Soap",
+    10767: "Talk",
+    10768: "War & Politics"
 }
 
 async def fetch_json(session, url, params):
