5ff455c2f852399bc7029b782069b9789b0b5d46
Shaquile Ellard Andhika <100258198+gtvservices5@users.noreply.github.com>
2025-10-21 06:19:35 -0500

    Update moveonjoy_updater.py

--- .github/scripts/moveonjoy_updater.py
+++ .github/scripts/moveonjoy_updater.py
@@ -3,7 +3,7 @@ import re
 import requests
 
 # Your M3U file path in the repo
-M3U_FILE_PATH = "IPTV-LIVE/PrimeVision/us.m3u"
+M3U_FILE_PATH = "PrimeVision/us.m3u"
 
 # Range of MoveOnJoy subdomains to test
 START = 2
